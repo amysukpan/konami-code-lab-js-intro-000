8f361409729bdde2866505b9d08e615fa41956c6
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-17 03:05:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,28 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+/*function keymap(key){
+  switch (key) {
+  case "ArrowUp": return 38
+  case "ArrowDoen": return 40
+  case "ArrowRight":return 39
+  case "ArrowLeft": return 37
+  default: return 0
+  }
+}*/
+
+let keyseq=0
+function init() {// your code here
+  document.body.addEvenListener('keydown',konami)
 }
+
+ function konami(e){
+   if (e.key === codes[keyseq]){
+     alert(e.key)
+     keyseq++
+     if (keyseq === codes.length){
+       alert("congratulations!")
+       keyseq=0
+      }
+     } else keyseq=0
+   }
